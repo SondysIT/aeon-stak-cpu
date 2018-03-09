c45ecf4196fb66971dc3f41ac7ffbd746a46a02d
SondysIT <37102047+SondysIT@users.noreply.github.com>
2018-03-10 00:16:08 +0100

    Update cryptonight.h

--- crypto/cryptonight.h
+++ crypto/cryptonight.h
@@ -8,21 +8,22 @@ extern "C" {
 #include <stddef.h>
 #include <inttypes.h>
 
-#define MEMORY  2097152 / 2
+#define MEMORY (1 << 19)
+#define ITER   ((1 << 20)/2)
 
-typedef struct {
-	uint8_t hash_state[224]; // Need only 200, explicit align
-	uint8_t* long_state;
-	uint8_t ctx_info[24]; //Use some of the extra memory for flags
-} cryptonight_ctx;
+    typedef struct {
+        uint8_t hash_state[224]; // Need only 200, explicit align
+        uint8_t* long_state;
+        uint8_t ctx_info[24]; //Use some of the extra memory for flags
+    } cryptonight_ctx;
 
-typedef struct {
-	const char* warning;
-} alloc_msg;
+    typedef struct {
+        const char* warning;
+    } alloc_msg;
 
-size_t cryptonight_init(size_t use_fast_mem, size_t use_mlock, alloc_msg* msg);
-cryptonight_ctx* cryptonight_alloc_ctx(size_t use_fast_mem, size_t use_mlock, alloc_msg* msg);
-void cryptonight_free_ctx(cryptonight_ctx* ctx);
+    size_t cryptonight_init(size_t use_fast_mem, size_t use_mlock, alloc_msg* msg);
+    cryptonight_ctx* cryptonight_alloc_ctx(size_t use_fast_mem, size_t use_mlock, alloc_msg* msg);
+    void cryptonight_free_ctx(cryptonight_ctx* ctx);
 
 #ifdef __cplusplus
 }
